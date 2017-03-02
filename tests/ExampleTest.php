d655780fb36c3aeb67ad716cadae85de40cb2f88
Sergey Sorokin <s.sorokin@progforce.com>
2017-03-02 12:21:32 +0300

    Add new tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,11 +2,24 @@
 
 namespace Isswp101\Persimmon;
 
+use Elasticsearch\ClientBuilder;
+use Isswp101\Persimmon\Models\Model;
+use Isswp101\Persimmon\Repository\ElasticsearchRepository;
+
 class ExampleTest extends \PHPUnit_Framework_TestCase
 {
     /** @group failing */
     public function testTrueIsTrue()
     {
+        $client = ClientBuilder::create()->build();
+        $repository = new ElasticsearchRepository($client);
+        $model = new Model();
+        $model->id = 'my_id';
+        $model->testField = 'abc';
+//        $repository->insert($model);
+
+        echo $model;
+
         $this->assertTrue(true);
     }
 }
---
+++ tests/Models/Model.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Isswp101\Persimmon\Models;
+
+use Isswp101\Persimmon\DI\Container;
+use Isswp101\Persimmon\Model\BaseModel;
+
+class Model extends BaseModel
+{
+    protected static function di(): Container
+    {
+        return null;
+    }
+}
