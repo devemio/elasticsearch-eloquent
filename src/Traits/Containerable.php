b98c4850d7de6ab9c652d49725e8075c71f3ae07
Sergey Sorokin <s.sorokin@progforce.com>
2017-03-02 12:32:04 +0300

    Improve __toString()

--- src/Traits/Containerable.php
+++ src/Traits/Containerable.php
@@ -44,7 +44,7 @@ trait Containerable
 
     public function __toString(): string
     {
-        return $this->toJson();
+        return static::class . $this->toJson();
     }
 
     public function jsonSerialize()
