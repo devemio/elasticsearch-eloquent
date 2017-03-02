bcba7d95c02c40000759780696ea10ed6dceae14
Sergey Sorokin <s.sorokin@progforce.com>
2017-03-02 11:46:53 +0300

    Update test environment

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Isswp101\Persimmon;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /** @group failing */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
