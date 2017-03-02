cfd4e3e7625a65cf54d4348d0bae4134570b919d
Sergey Sorokin <s.sorokin@progforce.com>
2017-03-02 14:56:15 +0300

    Use php 7 features

--- src/Helpers/Arr.php
+++ src/Helpers/Arr.php
@@ -6,6 +6,6 @@ class Arr
 {
     public static function get(array $array, $key, $default = null)
     {
-        return array_key_exists($key, $array) ? $array[$key] : $default;
+        return $array[$key] ?? $default;
     }
 }
