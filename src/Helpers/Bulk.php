7e635657a473eac89a1049460cd4f58c27b3da93
Sergey Sorokin <s.sorokin@progforce.com>
2017-03-20 11:46:06 +0300

    Bulk done

--- src/Helpers/Bulk.php
+++ src/Helpers/Bulk.php
@@ -2,16 +2,45 @@
 
 namespace Isswp101\Persimmon\Helpers;
 
-use Isswp101\Persimmon\DI\DI;
+use Elasticsearch\Client;
+use Exception;
+use Isswp101\Persimmon\CollectionParser\ElasticsearchCollectionParser;
 use Isswp101\Persimmon\Model\IElasticsearchModel;
 
 class Bulk
 {
     /**
+     * @param Client $client
      * @param IElasticsearchModel[] $models
+     * @param int $chunk
      */
-    public static function index(array $models)
+    public static function index(Client $client, array $models, int $chunk = 1000): void
     {
-        DI::make(DI::ELASTICSEARCH)->getRepository()->bulk($models);
+        $i = 1;
+        $params = ['body' => []];
+        foreach ($models as $model) {
+            try {
+                $id = $model->getPrimaryKey();
+            } catch (Exception $e) {
+                $id = $i++;
+            }
+            $collection = new ElasticsearchCollectionParser($model::getCollection());
+            $params['body'][] = [
+                'index' => [
+                    '_index' => $collection->getIndex(),
+                    '_type' => $collection->getType(),
+                    '_id' => $id
+                ]
+            ];
+            $params['body'][] = $model->toArray();
+            if ($i % $chunk == 0) {
+                $responses = $client->bulk($params);
+                $params = ['body' => []];
+                unset($responses);
+            }
+        }
+        if (!empty($params['body'])) {
+            $client->bulk($params);
+        }
     }
 }
